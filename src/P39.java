631df79c8f1e46c65deb1cabdec390949ae22232
Madhur345 <38024131+Madhur345@users.noreply.github.com>
2018-11-03 10:38:31 +0530

    hello

---
+++ src/P39.java
@@ -0,0 +1,14 @@
+
+public class P39 {
+
+	static int i =10;
+	public static void main(String[] args) {
+		System.out.println(P37.i);
+		int i =5;
+		System.out.println(i);
+		P37.i= 20;
+		System.out.println(P37.i);
+	}
+}
+
+
